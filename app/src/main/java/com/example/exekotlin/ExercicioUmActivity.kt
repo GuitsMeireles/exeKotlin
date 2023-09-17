f18c4afa28b09b4ac14766538198004f71bc3657
SeuHokage <batistaguilherme681@gmail.com>
2023-09-17 20:05:17 -0300

    start commit!!!

---
+++ app/src/main/java/com/example/exekotlin/ExercicioUmActivity.kt
@@ -0,0 +1,11 @@
+package com.example.exekotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExercicioUmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercicio_um)
+    }
+}
