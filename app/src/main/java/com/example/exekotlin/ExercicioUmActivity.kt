d9386926ec2dc8b2bf2afbfb4a18ed1b1b035001
SeuHokage <batistaguilherme681@gmail.com>
2023-09-17 22:16:41 -0300

    exercicio um feito

--- app/src/main/java/com/example/exekotlin/ExercicioUmActivity.kt
+++ app/src/main/java/com/example/exekotlin/ExercicioUmActivity.kt
@@ -1,11 +1,38 @@
 package com.example.exekotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.exekotlin.databinding.ActivityExercicioUmBinding
 
 class ExercicioUmActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityExercicioUmBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_exercicio_um)
+        binding = ActivityExercicioUmBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btVoltar.setOnClickListener {
+            finish()
+        }
+
+        binding.calculaButton.setOnClickListener {
+            val numero1 = binding.numero1.text.toString().toInt()
+            val numero2 = binding.numero2.text.toString().toInt()
+
+            val soma = numero1 + numero2
+            val subtracao = numero1 - numero2
+            val multiplicacao = numero1 * numero2
+            val divisao = if (numero2 != 0) {
+                numero1.toDouble() / numero2.toDouble()
+            } else {
+                0.0 // Evita dividir por zero
+            }
+
+            binding.somaTextView.text = "Soma: $soma"
+            binding.subtracaoTextView.text = "Subtração: $subtracao"
+            binding.multiplicacaoTextView.text = "Multiplicação: $multiplicacao"
+            binding.divisaoTextView.text = "Divisão: $divisao"
+        }
     }
 }
--- app/src/main/java/com/example/exekotlin/MainActivity.kt
+++ app/src/main/java/com/example/exekotlin/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.exekotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.exekotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btExe1.setOnClickListener{
+            val i = Intent(this, ExercicioUmActivity::class.java)
+            startActivity(i)
+        }
+
     }
 }
