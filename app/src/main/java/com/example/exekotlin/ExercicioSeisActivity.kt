f301c92780da9677b1c89096d8956c005074234a
SeuHokage <batistaguilherme681@gmail.com>
2023-09-19 04:08:25 -0300

    exercicio seis concluido

--- app/src/main/java/com/example/exekotlin/ExercicioSeisActivity.kt
+++ app/src/main/java/com/example/exekotlin/ExercicioSeisActivity.kt
@@ -2,56 +2,46 @@ package com.example.exekotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import com.example.exekotlin.databinding.ActivityExercicioSeisBinding
 
 class ExercicioSeisActivity : AppCompatActivity() {
 
+    private lateinit var editTextFruta: EditText
+    private lateinit var buttonAdicionar: Button
+    private lateinit var textViewLista: TextView
+
+    private val carrinhoDeCompras = mutableListOf<String>()
+
     private lateinit var binding: ActivityExercicioSeisBinding
 
-    private val itemList = mutableListOf<String>()
-
-
-    private val valuesList = mutableListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExercicioSeisBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnVoltar.setOnClickListener {
+        binding.btVolt.setOnClickListener {
             finish()
         }
 
-        val editText = binding.etAdd
-        val addButton = binding.btAdd
-        val textView = binding.tvLista
+        editTextFruta = binding.editTextFruta
+        buttonAdicionar = binding.buttonAdicionar
+        textViewLista = binding.textViewLista
 
-        addButton.setOnClickListener {
-            // Obtenha o valor do EditText
-            val inputValue = editText.text.toString().trim()
-
-            if (inputValue.isNotEmpty()) {
-                // Adicione o valor à lista
-                textView.add(inputValue)
-
-                // Atualize a TextView com a lista de itens
-                updateTextView(textView)
-
-                // Limpe o EditText após adicionar o valor
-                editText.text.clear()
+        buttonAdicionar.setOnClickListener {
+            val fruta = editTextFruta.text.toString()
+            if (fruta.isNotBlank()) {
+                carrinhoDeCompras.add(fruta)
+                editTextFruta.text.clear()
+                atualizarLista()
             }
         }
     }
 
-    private fun updateTextView(textView: TextView) {
-        // Construa uma string com a lista de itens
-        val itemsString = itemList.joinToString("\n")
-
-        // Exiba a lista de itens na TextView
-        textView.text = itemsString
+    private fun atualizarLista() {
+        val listaFrutas = carrinhoDeCompras.joinToString("\n")
+        textViewLista.text = listaFrutas
     }
-}
-
-private fun TextView.add(inputValue: String) {
-    TODO("Not yet implemented")
 }
